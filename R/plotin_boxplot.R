0b62f213b7f0b85f467c29e484b78f672a475aa3
Yann <yilboudo@gmail.com>
2021-11-16 23:34:11 -0500

    better comment for boxplot functin

--- R/plotin_boxplot.R
+++ R/plotin_boxplot.R
@@ -22,7 +22,7 @@
 #' plotin(data_tgca_tumor_stat, patients= data_tgca_tumor_stat$bcr_patient_barcode[grepl("TCGA-B*",data_tgca_tumor_stat$bcr_patient_barcode)] , metadata_col_num= "OS.time",
 #' metadata_col ="race", metadata_levels=c("WHITE","ASIAN") )
 
-#creates boxplot
+#creates boxplot for tgca sheet1
 
 plotin <- function(df=NULL, cancer_type=NULL, patients=NULL, metadata_col_num=NULL, metadata_col = NULL, metadata_levels=c(NULL,NULL)) {
 
